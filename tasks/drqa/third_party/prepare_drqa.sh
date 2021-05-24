1ce652c7d5055f3e7e934fb0cf384dabe0e844af
Jong-Ryul Lee <jongryul.lee@etri.re.kr>
2021-05-24 16:52:04 +0900

    smallfry is added
    Signed-off-by: Jong-Ryul Lee <jongryul.lee@etri.re.kr>

---
+++ tasks/drqa/third_party/prepare_drqa.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+pushd ./src/smallfry/third_party/DrQA
+pip install -r requirements.txt; python setup.py develop
+mkdir data
+popd
+
+pushd ./src/smallfry/third_party/DrQA/data
+wget https://www.dropbox.com/s/tdekiyr8r14ogtk/datasets.zip?dl=0
+mv datasets.zip?dl=0 datasets.zip
+unzip datasets.zip
+wget http://nlp.stanford.edu/data/glove.6B.zip
+unzip glove.6B.zip
+popd
--- tasks/drqa/third_party/setup.py
+++ tasks/drqa/third_party/setup.py
@@ -1,29 +1,18 @@
-#!/usr/bin/env python3
-# Copyright 2017-present, Facebook, Inc.
-# All rights reserved.
-#
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
+from setuptools import find_packages, setup
+from os.path import basename, splitext
+from glob import glob
 
-from setuptools import setup, find_packages
-import sys
-
-with open('README.md') as f:
-    readme = f.read()
-
-with open('LICENSE') as f:
-    license = f.read()
-
-with open('requirements.txt') as f:
-    reqs = f.read()
-
-setup(
-    name='drqa',
-    version='0.1.0',
-    description='Reading Wikipedia to Answer Open-Domain Questions',
-    long_description=readme,
-    license=license,
-    python_requires='>=3.5',
-    packages=find_packages(exclude=('data')),
-    install_requires=reqs.strip().split('\n'),
-)
+setup(name='smallfry',
+      version='0.1',
+      description='Code for smallfry.',
+      packages=find_packages("src"),
+      package_dir={"": "src"},
+      py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],
+      url='https://github.com/HazyResearch/smallfry.git',
+      author='Avner May / Jian Zhang',
+      author_email='zjian@stanford.edu',
+      license='Apache Version 2',
+      install_requires = ['numpy',
+                          'torch']
+                          
+      )
