de60f8057e10a85b836a21afe285cabcace71d2d
Jong-Ryul Lee <jongryul.lee@etri.re.kr>
2021-05-24 17:13:08 +0900

    snli is edited for discblock
    Signed-off-by: Jong-Ryul Lee <jongryul.lee@etri.re.kr>

--- tasks/snli/third_party/datasets/snli.py
+++ tasks/snli/third_party/datasets/snli.py
@@ -5,41 +5,56 @@ import torch
 
 from torchtext.data import Field, Iterator, TabularDataset
 from torchtext import datasets
-from utils import makedirs
 
 from pdb import set_trace
 
 __all__ = ['snli']
 
+def makedirs(name):
+        """helper function for python 2 and 3 to call os.makedirs()
+                avoiding an error if the directory to be created already exists"""
+
+        import os, errno
+
+        try:
+                os.makedirs(name)
+        except OSError as ex:
+                if ex.errno == errno.EEXIST and os.path.isdir(name):
+                        # ignore existing directory
+                        pass
+                else:
+                        # a different error happened
+                        raise
+
 class SNLI():
-	def __init__(self, options):
-		self.TEXT = Field(lower=True, tokenize='spacy', batch_first = True)
-		self.LABEL = Field(sequential=False, unk_token = None, is_target = True)
-		
-		self.train, self.dev, self.test = datasets.SNLI.splits(self.TEXT, self.LABEL)
-		
-		self.TEXT.build_vocab(self.train, self.dev)
-		self.LABEL.build_vocab(self.train)
+        def __init__(self, options):
+                self.TEXT = Field(lower=True, tokenize='spacy', batch_first = True)
+                self.LABEL = Field(sequential=False, unk_token = None, is_target = True)
+                
+                self.train, self.dev, self.test = datasets.SNLI.splits(self.TEXT, self.LABEL)
+                
+                self.TEXT.build_vocab(self.train, self.dev)
+                self.LABEL.build_vocab(self.train)
 
-		vector_cache_loc = '.vector_cache/snli_vectors.pt'
-		if os.path.isfile(vector_cache_loc):
-			self.TEXT.vocab.vectors = torch.load(vector_cache_loc)
-		else:
-			self.TEXT.vocab.load_vectors('glove.840B.300d')
-			makedirs(os.path.dirname(vector_cache_loc))
-			torch.save(self.TEXT.vocab.vectors, vector_cache_loc)
+                vector_cache_loc = '.vector_cache/snli_vectors.pt'
+                if os.path.isfile(vector_cache_loc):
+                        self.TEXT.vocab.vectors = torch.load(vector_cache_loc)
+                else:
+                        self.TEXT.vocab.load_vectors('glove.840B.300d')
+                        makedirs(os.path.dirname(vector_cache_loc))
+                        torch.save(self.TEXT.vocab.vectors, vector_cache_loc)
 
-		self.train_iter, self.dev_iter, self.test_iter = Iterator.splits((self.train, self.dev, self.test), 
-																													batch_size=options['batch_size'], device=options['device'])
+                self.train_iter, self.dev_iter, self.test_iter = Iterator.splits((self.train, self.dev, self.test), 
+                                                                                                                                                                                                                                        batch_size=options['batch_size'], device=options['device'])
 
-	def vocab_size(self):
-		return len(self.TEXT.vocab)
+        def vocab_size(self):
+                return len(self.TEXT.vocab)
 
-	def out_dim(self):
-		return len(self.LABEL.vocab)
+        def out_dim(self):
+                return len(self.LABEL.vocab)
 
-	def labels(self):
-		return self.LABEL.vocab.stoi
+        def labels(self):
+                return self.LABEL.vocab.stoi
 
 def snli(options):
-	return SNLI(options)
+        return SNLI(options)
--- tasks/snli/third_party/train.py
+++ tasks/snli/third_party/train.py
@@ -12,109 +12,152 @@ from torch.optim.lr_scheduler import StepLR
 import logging
 
 
-import datasets
-import models
+from tasks.snli import datasets
+from tasks.snli import models
 
-from utils import *
+from tasks.snli.utils import *
 from pdb import set_trace
 
 class Train():
-	def __init__(self):
-		print("program execution start: {}".format(datetime.datetime.now()))
-		self.args = parse_args()
-		self.device = get_device(self.args.gpu)
-		self.logger = get_logger(self.args, "train")
-		self.logger.info("Arguments: {}".format(self.args))
-		
-		dataset_options = {
-											'batch_size': self.args.batch_size, 
-											'device': self.device
-										}
-		self.dataset = datasets.__dict__[self.args.dataset](dataset_options)
-		
-		self.model_options = {
-									'out_dim': self.dataset.out_dim(),
-									'dp_ratio': self.args.dp_ratio,
-									'd_hidden': self.args.d_hidden,
-									'device': self.device,
-									'dataset': self.args.dataset
-								}
-		self.model = models.__dict__[self.args.model](self.model_options)
-		
-		self.model.to(self.device)
-		self.criterion = nn.CrossEntropyLoss(reduction = 'sum')
-		self.opt = O.Adam(self.model.parameters(), lr = self.args.lr)
-		self.best_val_acc = None
-		self.scheduler = StepLR(self.opt, step_size=5, gamma=0.5)
+        def __init__(self):
+                print("program execution start: {}".format(datetime.datetime.now()))
+                self.args = parse_args()
+                self.device = get_device(self.args.gpu)
+                self.logger = get_logger(self.args, "train")
+                self.logger.info("Arguments: {}".format(self.args))
+                
+                dataset_options = {
+                                                                                        'batch_size': self.args.batch_size, 
+                                                                                        'device': self.device
+                                                                                }
+                print(datasets.__dict__.keys())
+                self.dataset = datasets.__dict__[self.args.dataset](dataset_options)
+                
+                self.model_options = {
+                                                                        'out_dim': self.dataset.out_dim(),
+                                                                        'dp_ratio': self.args.dp_ratio,
+                                                                        'd_hidden': self.args.d_hidden,
+                                                                        'device': self.device,
+                                                                        'dataset': self.args.dataset
+                                                                }
+                self.model = models.__dict__[self.args.model](self.model_options)
+                self.model.to(self.device)
+                print("resource preparation done: {}".format(datetime.datetime.now()))
 
-		print("resource preparation done: {}".format(datetime.datetime.now()))
+        def result_checkpoint(self, epoch, train_loss, val_loss, train_acc, val_acc, took):
+                if self.best_val_acc is None or val_acc > self.best_val_acc:
+                        self.best_val_acc = val_acc
+                        torch.save({
+                                'accuracy': self.best_val_acc,
+                                'options': self.model_options,
+                                'model_dict': self.model.state_dict(),
+                        }, '{}/{}/{}/best-{}-{}-params.pt'.format(self.args.results_dir, self.args.model, self.args.dataset, self.args.model, self.args.dataset))
+                        torch.save({
+                                'accuracy': self.best_val_acc,
+                                'options': self.model_options,
+                                'model': self.model,
+                        }, '{}/{}/{}/best-{}-{}-params.pth'.format(self.args.results_dir, self.args.model, self.args.dataset, self.args.model, self.args.dataset))
+                self.logger.info('| Epoch {:3d} | train loss {:5.2f} | train acc {:5.2f} | val loss {:5.2f} | val acc {:5.2f} | time: {:5.2f}s |'
+                                .format(epoch, train_loss, train_acc, val_loss, val_acc, took))
+        
+        def train(self, config=None):
 
-	def result_checkpoint(self, epoch, train_loss, val_loss, train_acc, val_acc, took):
-		if self.best_val_acc is None or val_acc > self.best_val_acc:
-			self.best_val_acc = val_acc
-			torch.save({
-				'accuracy': self.best_val_acc,
-				'options': self.model_options,
-				'model_dict': self.model.state_dict(),
-			}, '{}/{}/{}/best-{}-{}-params.pt'.format(self.args.results_dir, self.args.model, self.args.dataset, self.args.model, self.args.dataset))
-		self.logger.info('| Epoch {:3d} | train loss {:5.2f} | train acc {:5.2f} | val loss {:5.2f} | val acc {:5.2f} | time: {:5.2f}s |'
-				.format(epoch, train_loss, train_acc, val_loss, val_acc, took))
-	
-	def train(self):
-		self.model.train(); self.dataset.train_iter.init_epoch()
-		n_correct, n_total, n_loss = 0, 0, 0
-		for batch_idx, batch in enumerate(self.dataset.train_iter):
-			self.opt.zero_grad()
-			answer = self.model(batch)
-			loss = self.criterion(answer, batch.label)
-			
-			n_correct += (torch.max(answer, 1)[1].view(batch.label.size()) == batch.label).sum().item()
-			n_total += batch.batch_size
-			n_loss += loss.item()
-			
-			loss.backward(); self.opt.step()
-		train_loss = n_loss/n_total
-		train_acc = 100. * n_correct/n_total
-		return train_loss, train_acc
+                self.criterion = nn.CrossEntropyLoss(reduction = 'sum')
+                if config is not None and "diff_embedding" in config["embedding"] and "gate_lr" in config["diff_embedding"]:
+                    if config["diff_embedding"]["gate_training_only"]:
+                        print("USE GATE LR:", config["diff_embedding"]["gate_lr"])
+                        lr = config["diff_embedding"]["gate_lr"]
+                    else:
+                        lr = self.args.lr
+                else:
+                    lr = self.args.lr
 
-	def validate(self):
-		self.model.eval(); self.dataset.dev_iter.init_epoch()
-		n_correct, n_total, n_loss = 0, 0, 0
-		with torch.no_grad():
-			for batch_idx, batch in enumerate(self.dataset.dev_iter):
-				answer = self.model(batch)
-				loss = self.criterion(answer, batch.label)
-				
-				n_correct += (torch.max(answer, 1)[1].view(batch.label.size()) == batch.label).sum().item()
-				n_total += batch.batch_size
-				n_loss += loss.item()
+                self.opt = O.Adam([p for p in self.model.parameters() if p.requires_grad], lr = lr)
+                self.best_val_acc = None
+                self.scheduler = StepLR(self.opt, step_size=5, gamma=0.5)
 
-			val_loss = n_loss/n_total
-			val_acc = 100. * n_correct/n_total
-			return val_loss, val_acc
+                self.model.train(); self.dataset.train_iter.init_epoch()
+                n_correct, n_total, n_loss = 0, 0, 0
+                for batch_idx, batch in enumerate(self.dataset.train_iter):
+                        self.opt.zero_grad()
+                        answer = self.model(batch)
+                        loss = self.criterion(answer, batch.label)
+                        
+                        n_correct += (torch.max(answer, 1)[1].view(batch.label.size()) == batch.label).sum().item()
+                        n_total += batch.batch_size
+                        n_loss += loss.item()
 
-	def execute(self):
-		print(" [*] Training starts!")
-		print('-' * 99)
-		for epoch in range(1, self.args.epochs+1):
-			start = time.time()
+                        sparsity_loss = 0.0
+                        for c in self.model.children():
+                            if type(c).__name__ == "DifferentiableEmbedding" or type(c).__name__ == "DifferentiableEmbeddingClassifier":
+                                sparsity_loss += c.get_sparsity_loss()
 
-			train_loss, train_acc = self.train()
-			val_loss, val_acc = self.validate()
-			self.scheduler.step()
-			
-			took = time.time()-start
-			self.result_checkpoint(epoch, train_loss, val_loss, train_acc, val_acc, took)
+                        (loss+sparsity_loss).backward(); self.opt.step()
 
-			print('| Epoch {:3d} | train loss {:5.2f} | train acc {:5.2f} | val loss {:5.2f} | val acc {:5.2f} | time: {:5.2f}s |'.format(
-				epoch, train_loss, train_acc, val_loss, val_acc, took))
-		self.finish()
+                        if config is not None and "diff_embedding" in config["embedding"] and "gate_clamping" in config["diff_embedding"]:
+                            gate_clamping = config["diff_embedding"]["gate_clamping"]
+                            for c in self.model.modules():
+                                if type(c).__name__ == "DifferentiableEmbedding":
+                                    #torch.clamp_(c.gates.weight.data, min=1.0, max=c.embedding.weight.size()[1])
+                                    if type(c.gates).__name__ == "Embedding":
+                                        torch.clamp_(c.gates.weight.data, min=gate_clamping[0], max=gate_clamping[1])
+                                    else:
+                                        torch.clamp_(c.gates.data, min=gate_clamping[0], max=gate_clamping[1])
+                                elif type(c).__name__ == "DifferentiableEmbeddingClassifier":
+                                    #torch.clamp_(c.gates.weight.data, min=1.0, max=c.weight.size()[0])
+                                    if type(c.gates).__name__ == "Embedding":
+                                        torch.clamp_(c.gates.weight.data, min=gate_clamping[0], max=gate_clamping[1])
+                                    else:
+                                        torch.clamp_(c.gates.data, min=gate_clamping[0], max=gate_clamping[1])
 
-	def finish(self):
-		self.logger.info("[*] Training finished!\n\n")
-		print('-' * 99)
-		print(" [*] Training finished!")
-		print(" [*] Please find the saved model and training log in results_dir")
+                train_loss = n_loss/n_total
+                train_acc = 100. * n_correct/n_total
+                return train_loss, train_acc
 
-task = Train()
-task.execute()
+        def validate(self):
+                if not hasattr(self, "criterion"):
+                    self.criterion = nn.CrossEntropyLoss(reduction = 'sum')
+                self.model.eval(); self.dataset.dev_iter.init_epoch()
+                n_correct, n_total, n_loss = 0, 0, 0
+                with torch.no_grad():
+                        for batch_idx, batch in enumerate(self.dataset.dev_iter):
+                                answer = self.model(batch)
+                                loss = self.criterion(answer, batch.label)
+                                
+                                n_correct += (torch.max(answer, 1)[1].view(batch.label.size()) == batch.label).sum().item()
+                                n_total += batch.batch_size
+                                n_loss += loss.item()
+
+                        val_loss = n_loss/n_total
+                        val_acc = 100. * n_correct/n_total
+                        return val_loss, val_acc
+
+        def execute(self, config=None, evaluate_gates=None):
+                print(" [*] Training starts!")
+                print('-' * 99)
+                for epoch in range(1, self.args.epochs+1):
+                        start = time.time()
+
+                        train_loss, train_acc = self.train(config)
+                        val_loss, val_acc = self.validate()
+                        self.scheduler.step()
+                        
+                        if config is not None and "use_eval_gates" in config and config["use_eval_gates"] and "diff_embedding" in config["embedding"]:
+                            val_acc = evaluate_gates(self.model)
+                            print("********************* eval gates: %f", val_acc)
+
+                        took = time.time()-start
+                        self.result_checkpoint(epoch, train_loss, val_loss, train_acc, val_acc, took)
+
+                        print('| Epoch {:3d} | train loss {:5.2f} | train acc {:5.2f} | val loss {:5.2f} | val acc {:5.2f} | time: {:5.2f}s |'.format(
+                                epoch, train_loss, train_acc, val_loss, val_acc, took))
+                self.finish()
+
+        def finish(self):
+                self.logger.info("[*] Training finished!\n\n")
+                print('-' * 99)
+                print(" [*] Training finished!")
+                print(" [*] Please find the saved model and training log in results_dir")
+
+#task = Train()
+#task.execute()
